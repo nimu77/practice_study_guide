70997a141ddc5907ddbc4dd4f3097debc87b2b7f
nimu77 <nirmal.thapa77@outlook.com>
2020-06-11 15:43:56 -0700

    part1-creating and inserting into table

---
+++ study_part1.py
@@ -0,0 +1,36 @@
+import sqlite3
+import os
+
+filepath = os.path.join('study_part1.sqlite3')
+
+
+connection = sqlite3.connect(filepath)
+
+cursor = connection.cursor()
+
+query = '''
+CREATE TABLE IF NOT EXISTS profile (
+    student_id SERIAL Primary Key,
+    student varchar,
+    studied varchar, 
+    grade int, 
+    age int, 
+    sex varchar
+);
+'''
+
+cursor.execute(query)
+# connection.commit()
+
+insertion_query = '''
+INSERT INTO profile VALUES 
+    (1, 'LION-O', 'True', 85, 24, 'Male'),
+    (2, 'Cheetara', 'True', 95, 22, 'Female'),
+    (3, 'Mumm-Ra', 'False', 65, 153, 'Male'), 
+    (4, 'Snarf', 'False', 70, 15, 'Male'), 
+    (5, 'Panthro', 'True', 80, 30, 'Male')
+'''
+
+cursor.execute(insertion_query)
+
+connection.commit()
